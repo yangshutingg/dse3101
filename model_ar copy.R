eaee421c90dc2040ef222c48d1b949990adb6fe5
kellywty <wangtingyukelly@gmail.com>
2024-03-31 22:27:08 +0800

    Read data directly, added starting quarter as input

--- model_ar copy.R
+++ model_ar copy.R
@@ -3,7 +3,12 @@ rm(list=ls())
 library(readxl)
 library(tidyverse)
 library(zoo)
-data = read_excel("../data/ROUTPUTQvQd.xlsx") 
+data = read_excel("ROUTPUTQvQd.xlsx") 
+data = data[-c(1:48), ] # remove data before 1959Q1 due to NAs
+
+# user input:
+# starting & ending quarter for testing(min 10 quarters?), h-step, expanding/rolling window for cv
+# starting must > 1959q1+60, ending must >= starting+10, 10 <= ending-starting <= 40 --> training cv testing ratio = 80:20:20
 
 # function: get necessary data 
 # input: user select the end of the desired forecast interval
@@ -12,14 +17,21 @@ data = read_excel("../data/ROUTPUTQvQd.xlsx")
 
 mse_data = data[,ncol(data)]
 
-get_data = function(end_quarter) {
-  col_name = paste("ROUTPUT", end_quarter, sep = "")
+get_data = function(start_quarter, end_quarter) {
+  start_col_name = paste("ROUTPUT", start_quarter, sep = "")
+  end_col_name = paste("ROUTPUT", end_quarter, sep = "")
+  
+  end_index = as.numeric(str_sub(end_quarter, start = 1, end = 2)) + as.numeric(str_sub(end_quarter, start = 4, end = 4)) / 4
+  # 24q1 --> 24.25
+  start_index = as.numeric(str_sub(start_quarter, start = 1, end = 2)) + as.numeric(str_sub(start_quarter, start = 4, end = 4)) / 4
+  num_quarters <<- (end_index - start_index) * 4
   
   data_full = data %>%
-    select("DATE" : col_name)
+    select("DATE" : end_col_name)
   
-  # extract last 21 columns up til the end period: 1 column for training, 10 in cross validation, 10 in testing
-  data_full = data_full[, (ncol(data_full)-21+1):ncol(data_full)]
+  num_col = num_quarters * 2 + 1
+  # extract last num_col columns up til the end period: 1 column for training, num_quarters in cross validation, num_quarters in testing
+  data_full = data_full[, (ncol(data_full)-num_col+1):ncol(data_full)]
   
   # adding the date column
   data_full = cbind(data$DATE, data_full)
@@ -27,16 +39,16 @@ get_data = function(end_quarter) {
   return(data_full)
 }
 
-data_full = get_data("24Q1")
+data_full = get_data("20Q1","24Q1")
 
 year_end = as.numeric(str_sub("24Q1", start = 1, end = 2))
 quarter_end = as.numeric(str_sub("24Q1", start = 4, end = 4))
 ll = get_data("14Q1") %>% select(last_col()) %>% rename_with(.cols = 1, ~"gdp") %>%
-  filter(!is.na(as.numeric(gdp))) %>% nrow()
+  filter(!is.na(as.numeric("gdp"))) %>% nrow()
 true_values = tail(mse_data[1:ll,], 15)
 data2 = get_data("12Q3")
 get_data("24Q1") %>% select(last_col()) %>% rename_with(.cols = 1, ~"gdp") %>%
-  filter(is.na(as.numeric(gdp)))
+  filter(is.na(as.numeric("gdp")))
 
 fitAR=function(Y,p,h){
   
@@ -70,6 +82,7 @@ fitAR=function(Y,p,h){
 # output: fitted training model, in a list
 # take the most recent data(column), retrieve the latest 80 quarters as training data
 
+num_quarters_training = num_quarters * 4
 trainAR = function(data_full, p, h) {
   data_training = data_full %>%
     select(1:2) %>%
@@ -77,7 +90,7 @@ trainAR = function(data_full, p, h) {
     rename_with(.cols = 2, ~"gdp") %>%  # renaming columns
     mutate(gdp = as.numeric(gdp)) %>%
     drop_na() %>%
-    tail(n = 80) %>%
+    tail(n = num_quarters_training) %>%
     mutate(loggdp = log(gdp))
   
   temp=embed(data_training$loggdp,2) #create lag of log(GDP) and align the original series
@@ -159,15 +172,15 @@ ar.rolling.window=function(data_cv,noos,p,h=1){ #equality here  means default in
 
 cv_rolling = function(data_full, noos = 10, p, h = 1){
   data_cv = data_full %>%
-    select(c(1, 3:12))
+    select(c(1, 3:(num_quarters+2)))
   return(ar.rolling.window(data_cv, noos, p, h))
 }
 
-ar12=cv_rolling(data_full,10,2,1) #1-step POOS AR(2) forecast
+ar12=cv_rolling(data_full,num_quarters,2,1) #1-step POOS AR(2) forecast
 
 test_rolling = function(data_full, noos = 10, p, h){
   data_test = data_full %>%
-    select(c(1, 13:22))
+    select(c(1, (num_quarters+3):ncol(data_full)))
   return(ar.rolling.window(data_test, noos, p, h))
 }
 
@@ -217,15 +230,15 @@ ar.expanding.window=function(data_cv,noos,p,h=1){ #equality here  means default 
 
 cv_expanding = function(data_full, noos = 10, p, h = 1){
   data_cv = data_full %>%
-    select(c(1, 3:12))
+    select(c(1, 3:(num_quarters+2)))
   return(ar.expanding.window(data_cv, noos, p, h))
 }
 
-ar1.1=cv_expanding(data_full,10,2,1) #1-step POOS AR(2) forecast
+ar1.1=cv_expanding(data_full,num_quarters,2,1) #1-step POOS AR(2) forecast
 
 test_expanding = function(data_full, noos = 10, p, h){
   data_test = data_full %>%
-    select(c(1, 13:22))
+    select(c(1, (num_quarters+3):ncol(data_full)))
   return(ar.expanding.window(data_test, noos, p, h))
 }
 
@@ -243,10 +256,10 @@ intervals = function(x, p, rmsfe) {
 int = intervals(ar1.1$pred, 0.95, ar1.1$errors[1])
 int[,3]
 int$lower
-# true_ts = ts(tail(Y, 15), start = c(20, 3), end = c(24, 1), frequency = 4)
-# forecast.ts = ts(ar1.1$pred, start = c(21, 4), end = c(24, 1), frequency = 4)
-# forecast.ts1 = ts(ar1.1$pred, start = c(20, 3), end = c(24, 1), frequency = 4)
-# plot.ts(true_ts)
-# points(forecast.ts, type = "l", col = "red")
-# points(forecast.ts)
-# plot(forecast.ts)
+true_ts = ts(tail(Y, 15), start = c(20, 3), end = c(24, 1), frequency = 4)
+forecast.ts = ts(ar1.1$pred, start = c(21, 4), end = c(24, 1), frequency = 4)
+forecast.ts1 = ts(ar1.1$pred, start = c(20, 3), end = c(24, 1), frequency = 4)
+plot.ts(true_ts)
+points(forecast.ts, type = "l", col = "red")
+points(forecast.ts)
+plot(forecast.ts)
