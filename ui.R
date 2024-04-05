5376c62530dd4eea5b13cdcea24d77338ef24460
yangshutingg <e0773572@u.nus.edu>
2024-04-05 22:33:26 +0800

    update inputs

--- ui.R
+++ ui.R
@@ -2,21 +2,6 @@ library(shiny)
 library(shinyWidgets)
 
 ui <- fluidPage(
-  selectInput("model_type",
-              "Model type",
-              choices = c("AR", "ADL", "PCR")),
-  selectInput("h",
-              "Forecast horizon",
-              choices = c("1", "2", "3", "4")
-  ),
-  selectInput("window",
-              "Window type",
-              choices = c("Rolling", "Expanding")
-  ),
-  selectInput("sig_level",
-              "Forecast interval",
-              choices = c("50%", "68%", "80%", "90%")
-  ),
   sliderTextInput("start_quarter",
                   "Starting quarter",
                   choices = data$DATE,
@@ -25,11 +10,27 @@ ui <- fluidPage(
                   to_max = "2023:Q3"
   ),
   sliderTextInput("end_quarter",
-              "Ending quarter",
-              choices = data$DATE,
-              selected = "2003:Q3",
-              from_min = "2003:Q3",
-              to_max = "2023:Q4"
+                  "Ending quarter",
+                  choices = data$DATE,
+                  selected = "2003:Q3",
+                  from_min = "2003:Q3",
+                  to_max = "2023:Q4"
+  ),
+  selectInput("h",
+              "Forecast horizon",
+              choices = c("1", "2", "3", "4")
+  ),
+  selectInput("model_type",
+              "Model type",
+              choices = c("AR", "ADL", "PCR")
+  ),
+  selectInput("window",
+              "Window type",
+              choices = c("Rolling", "Expanding")
+  ),
+  selectInput("sig_level",
+              "Forecast interval",
+              choices = c("50%", "68%", "80%", "90%")
   ),
   textOutput("rmsfe"),
   textOutput("mae"),
