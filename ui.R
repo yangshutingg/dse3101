4c1a8db46b904c176ed40ee97cbfdea992e504dc
yangshutingg <e0773572@u.nus.edu>
2024-04-10 09:08:30 +0800

    update title panel

--- ui.R
+++ ui.R
@@ -113,7 +113,7 @@ table>thead>tr>th, .table>thead>tr>th, table>tbody>tr>th, .table>tbody>tr>th, ta
     tags$link(href = 'https://fonts.googleapis.com/css?family=Arial', rel = 'stylesheet', type = 'text/css')
   ),
 
-  titlePanel("Real-Time GDP Growth Forecast"),
+  titlePanel("REAL-TIME GDP GROWTH FORECAST"),
   shinythemes::themeSelector(),
   sidebarLayout(
     sidebarPanel(
